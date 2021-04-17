60393e72d2ad3722901e1210f9b47d3d1919c25d
Leonardo / LuKi <leo.monteiro06@live.com>
2021-04-17 19:33:40 -0300

    Bot 1.0

---
+++ comandos/brainly.js
@@ -0,0 +1,31 @@
+const { BrainlyAPI, Server } = require('brainly-api');
+
+module.exports = {
+    name: "b",
+    description: "brainly",
+    async execute(message) {
+      if(message.content[0] === '!' && message.content[1] === 'b') {
+        let question = message.content.substring(message.content.indexOf(" ") + 1, message.content.length);
+        BrainlyAPI.startWorker({ experimental: true, server: Server.PT }, async brainly => {
+          const resp = await brainly.findQuestion(question);
+          const id = (resp.raw[0].data.questionSearch.edges[0].node.databaseId);
+          const url = `https://brainly.com.br/tarefa/${id}`;
+          const htmlAnswer = resp.raw[0].data.questionSearch.edges[0].node.answers.nodes[0].content;
+          const answer = htmlAnswer.replace( /(<([^>]+)>)/ig, '');
+          const htmlQ = resp.raw[0].data.questionSearch.edges[0].node.content;
+          const q = htmlQ.replace( /(<([^>]+)>)/ig, '');
+
+          message.channel.send('Questão: \n');
+          message.channel.send(q);
+          message.channel.send('Resposta: \n');
+          message.channel.send(answer);
+          message.channel.send('Link: \n');
+          message.channel.send(url);
+
+        });
+      } else {
+        message.channel.send('Eita, não achei nada');
+      }
+
+    }
+  };
---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+
+const token = "";
+const prefix = "!";
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./comandos').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./comandos/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Vamo q vamo!');
+  client.user.setActivity(`Em operação!`, { type: "LISTENING" });
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('Vixe, tem algo errado');
+	}
+});
+
+client.login(token);
